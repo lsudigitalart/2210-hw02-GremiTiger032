847c8102e503c3eb774a0083f085251e0d0c3d38
GremiTiger032 <bgremi1@lsu.edu>
2024-09-10 22:36:00 -0500

    final product
    done

--- sketch.js
+++ sketch.js
@@ -1,7 +1,89 @@
 function setup() {
-    createCanvas(400, 100);
-    background(150);
+    createCanvas(400, 269);
 }
 
 function draw() {
+
+background(0);
+
+ // noStroke();
+ strokeWeight(5);
+fill(2);
+ rect(1, 30, 10000, 7);
+
+ strokeWeight(5);
+ fill(2);
+ rect(1, 60, 10000);
+
+ strokeWeight(5);
+ fill(255,202,0)
+ellipse(2,390,650,600)
+
+strokeWeight(5);
+fill(255,202,0)
+ellipse(40,390,150,600)
+
+ strokeWeight(5);
+ fill(255,202,0)
+ellipse(300,300,650,600)
+
+strokeWeight(18);
+fill(255,202,0)
+ellipse(160,440,360,600)
+
+strokeWeight(9);
+fill(255,202,0)
+ellipse(310,350,400,570)
+fill(255,202,0)
+ellipse(300,360,400,570)
+fill(255,202,0)
+ellipse(320,360,400,570)
+fill(255,202,0)
+ellipse(310,370,400,570)
+
+strokeWeight(9);
+fill(255,202,0)
+ellipse(320,380,400,570)
+
+strokeWeight(5);
+fill(255,202,0)
+ellipse(220,430,500,570)
+
+strokeWeight(5);
+fill(255,202,0)
+ellipse(310,450,650,600)
+
+strokeWeight(18);
+fill(255,202,0)
+ellipse(400,480,500,570)
+
+strokeWeight(4);
+fill(255,202,0)
+ellipse(450,480,500,570)
+fill(255,202,0)
+ellipse(460,480,500,570)
+fill(255,202,0)
+ellipse(490,480,500,570)
+fill(255,202,0)
+ellipse(500,480,500,570)
+strokeWeight(25);
+fill(255,202,0)
+ellipse(530,480,500,570)
+
+strokeWeight(5);
+fill(150,0,0);
+rect(1,1,460, 60,)
+
+strokeWeight(5);
+fill(2)
+ rect(1, 50, 10000, 4);
+
+ strokeWeight(5);
+ fill(2);
+ rect(1, 30, 10000, 7);
+ 
+ 
+ 
+
+
 }
